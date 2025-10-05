9cf24f83b80a866425b380131c123502f612a01a
browserandcreator <tigerxin024@gmail.com>
2025-10-05 21:46:12 +0800

    test script_v2

--- leetcode/problems/0135-candy/cpp/v1-solution.cpp
+++ leetcode/problems/0135-candy/cpp/v1-solution.cpp
@@ -39,9 +39,30 @@
  * Approach: TODO
  * Time: O(?), Space: O(?)
  */
+#include <vector>
+#include <algorithm>
+using namespace std;
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        
+        int n = ratings.size();
+        // 初始化vector容器为长度为n，初始值为1
+        vector<int> candies(n, 1);
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        int totalCandies = 0;
+        for (int c : candies) {
+            totalCandies += c;
+        }
+        return totalCandies;
     }
 };
